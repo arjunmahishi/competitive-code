02589d00b1b38fbe442636704e0f5e2d8f4650c5
Arjun Mahishi <arjun.mahishi@gmail.com>
2020-08-01 10:00:00 +0530

    Add 'Camel case' from code wars

---
+++ problems/codewars/go/camelCase.go
@@ -0,0 +1,15 @@
+// Link: https://www.codewars.com/kata/517abf86da9663f1d2000003/train/go
+
+package kata
+
+import "strings"
+
+func ToCamelCase(s string) string {
+	if len(s) < 1 {
+		return ""
+	}
+	return strings.Replace(
+		s[0:1]+strings.Title(strings.Replace(strings.Replace(s, "_", " ", -1), "-", " ", -1))[1:],
+		" ", "", -1,
+	)
+}
---
+++ problems/codewars/go/camelCase_test.go
@@ -0,0 +1,36 @@
+// Link: https://www.codewars.com/kata/517abf86da9663f1d2000003/train/go
+
+package kata
+
+import "testing"
+
+func TestToCamelCase(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			args: args{""},
+			want: "",
+		},
+		{
+			args: args{"The_Stealth_Warrior"},
+			want: "TheStealthWarrior",
+		},
+		{
+			args: args{"the-Stealth-Warrior"},
+			want: "theStealthWarrior",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToCamelCase(tt.args.s); got != tt.want {
+				t.Errorf("ToCamelCase() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
